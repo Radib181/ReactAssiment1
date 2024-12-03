5565512977b0572d69921ccc8e500c6e28e2e234
Radib <radibbhbh@gmail.com>
2024-12-03 16:25:57 +0600

    Design Cabge

--- src/assets/Comonent/Company.jsx
+++ src/assets/Comonent/Company.jsx
@@ -21,35 +21,35 @@ function Company() {
           <img
             src={Back}
             alt="Back"
-            className="w-20 h-20 object-contain shadow-lg rounded-lg transition duration-300 ease-in-out transform hover:scale-105"
+            className="w-20 h-20 object-contain  rounded-lg transition duration-300 ease-in-out transform hover:scale-105"
           />
         </div>
         <div className="flex justify-center items-center">
           <img
             src={Cablebs}
             alt="Cablebs"
-            className="w-28 h-20 object-contain shadow-lg rounded-lg transition duration-300 ease-in-out transform hover:scale-105"
+            className="w-28 h-20 object-contain  rounded-lg transition duration-300 ease-in-out transform hover:scale-105"
           />
         </div>
         <div className="flex justify-center items-center">
           <img
             src={Dbs}
             alt="Dbs"
-            className="w-28 h-20 object-contain shadow-lg rounded-lg transition duration-300 ease-in-out transform hover:scale-105"
+            className="w-28 h-20 object-contain  rounded-lg transition duration-300 ease-in-out transform hover:scale-105"
           />
         </div>
         <div className="flex justify-center items-center">
           <img
             src={Easy}
             alt="Easy"
-            className="w-28 h-20 object-contain shadow-lg rounded-lg transition duration-300 ease-in-out transform hover:scale-105"
+            className="w-28 h-20 object-contain  rounded-lg transition duration-300 ease-in-out transform hover:scale-105"
           />
         </div>
         <div className="flex justify-center items-center">
           <img
             src={Amd}
             alt="Amd"
-            className="w-28 h-20 object-contain shadow-lg rounded-lg transition duration-300 ease-in-out transform hover:scale-105"
+            className="w-28 h-20 object-contain rounded-lg transition duration-300 ease-in-out transform hover:scale-105"
           />
         </div>
       </div>
